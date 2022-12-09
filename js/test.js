48c49fb4f9e18afb8ab1d50af9e1be33895aed99
Zach Wells <zach.13.wells@gmail.com>
2022-12-08 20:11:05 -0600

    got size change working

--- js/test.js
+++ js/test.js
@@ -1,14 +1,20 @@
 var hexagon = new Path.RegularPolygon(new Point(50, 50), 6, 25);
 hexagon.fillColor = 'blue';
 hexagon.strokeColor = 'black';
-var growthRate;
+
+var text = new PointText(new Point(50, 100));
+text.content = hexagon.rotation;
+var growthRate = 0.99;
 
 function onFrame(event) {
-	if (hexagon.radius > 50) {
-		growthRate = -3;
-	}
-	if (hexagon.radius < 15) {
-		growthRate = 3;
-	}
-	hexagon.radius += growthRate;
+		hexagon.fillColor.hue += 1;
+		hexagon.rotate(1);
+		text.content = hexagon.bounds.width;
+		if (hexagon.bounds.width > 50) {
+				growthRate = 0.99;
+		}
+		if (hexagon.bounds.width < 25) {
+				growthRate = 1.01;
+		}
+		hexagon.scale(growthRate);
 }
